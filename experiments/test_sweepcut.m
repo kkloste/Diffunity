164822740772c9c7fde67215e1b87da441ca484c
“Kyle <kyle.kloster@gmail.com>
2016-06-30 16:49:00 -0400

    Testing sweepcut.m and sweepcut_mex

--- experiments/test_sweepcut.m
+++ experiments/test_sweepcut.m
@@ -8,6 +8,8 @@ n = size(A,1);
 
 for vert = 1:n,
 	[bestset,cond,cut,vol,prvec] = pprgrow_mex(A,vert, nnz(A)/6 ,0.85);
-	[bset, cond1, cut1, vol1] = sweepcut(A,prvec);
-	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'Failure on $d,  cond_pr = %f  v  cond1 = %f \n', vert, cond, cond1 ) );
+	densepr = zeros(n,1);
+	densepr( prvec(:,1), prve(:,2) );
+	[bset, cond1, cut1, vol1] = sweepcut(A,densepr);
+	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'Failure on %d,  cond_pr = %f  v  cond1 = %f \n', vert, cond, cond1 ) );
 end
