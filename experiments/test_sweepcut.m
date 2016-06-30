578259e5d215ed31b3d054473214cc4ec670c987
“Kyle <kyle.kloster@gmail.com>
2016-06-30 16:57:15 -0400

    Debugging sweepcut and test_sweepcut

--- experiments/test_sweepcut.m
+++ experiments/test_sweepcut.m
@@ -7,9 +7,16 @@ addpath ../util;
 n = size(A,1);
 
 for vert = 1:n,
-	[bestset,cond,cut,vol,prvec] = pprgrow_mex(A,vert, nnz(A)/6 ,0.85);
+	[bestset,condu,cut,vol,prvec] = pprgrow_mex(A,vert, nnz(A)/6 ,0.85);
 	densepr = zeros(n,1);
 	densepr( prvec(:,1) ) = prvec(:,2) ;
 	[bset, cond1, cut1, vol1] = sweepcut(A,densepr);
-	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'Failure on %d,  cond_pr = %f  v  cond1 = %f \n', vert, cond, cond1 ) );
+	[cond2 cut2 vol2] = cut_cond(A,bset);
+%	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'Failure on %d,  cond_pr = %f  v  cond1 = %f \n', vert, condu, cond1 ) );
+	fprintf( ' cond \t cut \t vol \n' );
+	fprintf( ' %f   \t %f  \t %f \n', condu, cut, vol );
+	fprintf( ' %f   \t %f  \t %f \n', cond1, cut1, vol1 );
+	fprintf( ' %f   \t %f  \t %f \n', cond2, cut2, vol2 );
+	fprintf( ' setdiff size %d \n', numel( setdiff( bset, bestset) ) );
+
 end
