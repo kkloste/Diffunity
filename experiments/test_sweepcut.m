556016f89852d6a28878558ca917349f6925ee8a
“Kyle <kyle.kloster@gmail.com>
2016-07-01 13:57:19 -0400

    sweepcut_mex appears to be working properly, returning to debugging test_sweepcut

--- experiments/test_sweepcut.m
+++ experiments/test_sweepcut.m
@@ -17,7 +17,7 @@ disp( [n, nnz(A)] )
 for vert = 1:n,
 	[bestset,condu,cut,vol,prvec] = pprgrow_mex(A,vert, nnz(A)/6 ,0.85);
 	prvec = sparse( prvec(:,1), 1, prvec(:,2), n, 1);
-	[bset, cond1, cut1, vol1, noderank] = sweepcut(A,prvec, 1);
+	[bset, cond1, cut1, vol1, noderank] = sweepcut(A,prvec);
 	[cond2 cut2 vol2] = cut_cond(A,bset);
 	[cond3 cut3 vol3] = cut_cond(A,bestset);
 
@@ -28,9 +28,9 @@ for vert = 1:n,
 %	assert( condu - cond3 == 0, sprintf( 'node = %d \t pprgrow - cut_cond = %f .', vert, condu-cond3 ));
 %	assert( cond1 - cond2 == 0, sprintf( 'node = %d \t sweepcut - cut_cond = %f .', vert, cond1-cond2 ));
 
-%	if jsim ~= 0 , sprintf( 'node = %d \t jac sim = %f ', vert, jsim ) ; end
-%	if condu - cond1 ~= 0, sprintf( 'node = %d \t pprgrow - sweepcut = %f .', vert, condu-cond1 ) ; end
-%	if condu - cond3 ~= 0, sprintf( 'node = %d \t pprgrow - cut_cond = %f .', vert, condu-cond3 ); end
+	if jsim ~= 0 , sprintf( 'node = %d \t jac sim = %f ', vert, jsim ) ; end
+	if condu - cond1 ~= 0, sprintf( 'node = %d \t pprgrow - sweepcut = %f .', vert, condu-cond1 ) ; end
+	if condu - cond3 ~= 0, sprintf( 'node = %d \t pprgrow - cut_cond = %f .', vert, condu-cond3 ); end
 	if cond1 - cond2 ~= 0, sprintf( 'node = %d \t sweepcut - cut_cond = %f .', vert, cond1-cond2 ); end
 
 	[vert length(noderank) nnz(prvec) length(bset)]
