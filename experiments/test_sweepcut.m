eb71e74277b430359d44ec890fc96a39c7a086cc
“Kyle <kyle.kloster@gmail.com>
2016-07-01 12:16:35 -0400

    Changing output of test_sweepcut to better debug

--- experiments/test_sweepcut.m
+++ experiments/test_sweepcut.m
@@ -1,6 +1,11 @@
 % Testing sweepcut
 
 load ../data/netscience-cc.mat;
+
+A = spones(A);
+assert( norm(A-A',1) == 0 , 'Not symmetric\n.');
+A = A - diag(A);
+
 addpath ../diffusion_codes;
 addpath ../util;
 
@@ -15,7 +20,7 @@ for vert = 1:n,
 	[cond3 cut3 vol3] = cut_cond(A,bestset);
 %	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'Failure on %d,  cond_pr = %f  v  cond1 = %f \n', vert, condu, cond1 ) );
 	fprintf( ' %f   \t %f  \t %f \n', cond1-condu, cut1-cut, vol1-vol );
-	fprintf( ' %f   \t %f  \t %f \n', cond1-cond2, cut1-cut1, vol1-vol1 );
+	fprintf( ' %f   \t %f  \t %f \n', cond1-cond2, cut1-cut2, vol1-vol2 );
 	fprintf( ' %f   \t %f  \t %f \n', cond3-condu, cut3-cut, vol3-vol );
 	fprintf( ' setdiff size %d \n', numel( setdiff( bset, bestset) ) );
 
