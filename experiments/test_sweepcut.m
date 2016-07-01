22686a6df72e8802e32f5b8e8a3b6f258287c937
“Kyle <kyle.kloster@gmail.com>
2016-07-01 12:27:53 -0400

    Changing output of test_sweepcut to better debug

--- experiments/test_sweepcut.m
+++ experiments/test_sweepcut.m
@@ -12,16 +12,19 @@ addpath ../util;
 n = size(A,1);
 degrees = full(sum(A,2));
 
+disp( [n, nnz(A)] )
+
 for vert = 1:n,
 	[bestset,condu,cut,vol,prvec] = pprgrow_mex(A,vert, nnz(A)/6 ,0.85);
 	prvec = sparse( prvec(:,1), 1, prvec(:,2), n, 1);
 	[bset, cond1, cut1, vol1] = sweepcut(A,prvec);
 	[cond2 cut2 vol2] = cut_cond(A,bset);
 	[cond3 cut3 vol3] = cut_cond(A,bestset);
-%	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'Failure on %d,  cond_pr = %f  v  cond1 = %f \n', vert, condu, cond1 ) );
-	fprintf( ' %f   \t %f  \t %f \n', cond1-condu, cut1-cut, vol1-vol );
-	fprintf( ' %f   \t %f  \t %f \n', cond1-cond2, cut1-cut2, vol1-vol2 );
-	fprintf( ' %f   \t %f  \t %f \n', cond3-condu, cut3-cut, vol3-vol );
-	fprintf( ' setdiff size %d \n', numel( setdiff( bset, bestset) ) );
+
+
+	assert( numel(setdiff(bestset,bset)) == 0 , fprintf( 'node = %d \t setdiff size = %d  \n', vert, numel(setdiff(bestset,bset))   ) );
+	assert( condu - cond1 == 0, fprintf( 'node = %d \t pprgrow - sweepcut = %f \n.', vert, condu-cond1 ) );
+	assert( condu - cond3 == 0, fprintf( 'node = %d \t pprgrow - cut_cond = %f \n.', vert, condu-cond3 ));
+	assert( cond1 - cond2 == 0, fprintf( 'node = %d \t sweepcut - cut_cond = %f \n.', vert, cond1-cond2 ));
 
 end
