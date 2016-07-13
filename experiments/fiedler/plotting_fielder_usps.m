37f82fa1d3174042b374319ce9a4fd224f9ecb33
“Kyle <kyle.kloster@gmail.com>
2016-07-12 23:15:10 -0400

    trying fiedler usps again

--- experiments/fiedler/plotting_fielder_usps.m
+++ experiments/fiedler/plotting_fielder_usps.m
@@ -5,23 +5,22 @@ load ../../data/usps3nn.mat ;
 
 clf;
 
-markersize = 6;
+markersize = 4;
 
 spy(A,'k', markersize);
 title('sparsity pattern, USPS');
 print( './images/usps3nn_spy','-dpng','-r600','-painters');
 
-%for j=1:length(bestset),
 hold all
 
-for j=1:20,
+for j=1:length(bestset),
 	vert = bestset(j);
 	neighb = A(:,vert);
 	neighb_in = intersect(neighb,bestset);
 	scatter( neighb_in, vert*ones(length(neighb_in),1 ), markersize, 'b', 'filled' );
 	scatter( vert*ones(length(neighb_in),1 ), neighb_in, markersize, 'b', 'filled' );
 
-	neighb_out = setdiff(neighb, neighb_in);
+	neighb_out = setdiff(neighb, neighb_out);
 	scatter( neighb_out, vert*ones(length(neighb_out), 1 ), markersize, 'r', 'filled' );
 	scatter( vert*ones(length(neighb_out), 1 ), neighb_out, markersize, 'r', 'filled' );
 
